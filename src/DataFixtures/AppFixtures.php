bc3f15161958082d5b5feac84bec9fa326b45436
Francis <francist@hotmail.com>
2020-10-12 18:42:35 +0200

    users and link with ads

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,16 +4,53 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
 
+        // Nous gérons les utilisateurs
+        $users = [];
+        $genres = ['male', 'female'];
+
+        for($i = 1; $i <= 10; $i++) {
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1, 99) . '.jpg';
+
+            $picture .= ($genre == 'male' ? 'men/' : 'women/') . $pictureId;
+
+            $password = $this->encoder->encodePassword($user, 'password');
+            
+            $user->setFirstName($faker->firstname($genre))
+                 ->setLastName($faker->lastname)
+                 ->setEmail($faker->email)
+                 ->setIntroduction($faker->sentence())
+                 ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>')
+                 ->setPassword($password)
+                 ->setPicture($picture);
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Nous gérons les annonces
         for($i = 1; $i <= 20; $i++) {
             $ad = new Ad();
 
@@ -24,6 +61,8 @@ class AppFixtures extends Fixture
             $name = $faker->firstNameMale();
             $city = $faker->city();
 
+            $user = $users[mt_rand(0, count($users) -1)];
+
             $ad->setType($type)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
@@ -32,7 +71,8 @@ class AppFixtures extends Fixture
                 ->setName($name)
                 ->setSexe(mt_rand(1, 2))
                 ->setCity($city)
-                ->setSize(mt_rand(1, 3));
+                ->setSize(mt_rand(1, 3))
+                ->setAuthor($user);;
 
                 for($j = 1; $j <= mt_rand(2, 5); $j++) {
                     $image = new Image();
